956476bc789dbbda590fba1e0886418a0c0084aa
João Pedro <127873652+joaoPedroPrograma@users.noreply.github.com>
2025-04-13 17:21:16 -0300

    aula 4 lista.py

--- aula 4 lista.py
+++ aula 4 lista.py
@@ -1,32 +1,42 @@
 #1
 def elegibilidade(idade):
+    '''
+    int => bool
+    '''
     verificador = idade >=18
     return f"A pessoa é elegível para votar? {verificador}"
 
 #2
 def bissextualidade(ano):
+    '''
+    int => bool
+    '''
     verificador = ano %4 == 0 and (ano % 100!= 0 or ano %400 == 0)
     return f"O ano é bissexto? {verificador}"
 
 #3
 def pontoCirculo(x,y,r):
+    '''
+    float, float, float => bool
+    '''
     return x**2 + y**2 <= r**2
 #4
 def coracaoDeMae(friends, carros):
+    '''
+    int, int => bool
+    '''
     assentos = 5 * carros
     verificador = friends <=assentos
     return verificador
 #5
 #6
 def botaCasacoTiraCasaco(temp):
-    if temp < 10:
-        return "Muito frio"
-    elif 10 >= temp <=17:
-        return "Frio"
-    elif 18 >= temp <=25:
-        return "Agradável"
-    elif 26 >= temp <= 35:
-        return "Quente"
-    else:
-        return "Muito quente" 
+    '''
+    float => str
+    '''
+    return ((temp < 10) * 'Muito frio' + (10 <= temp <= 17) * 'Frio' +
+        (18 <= temp <= 25) * 'Agradável' + (26 <= temp <= 35) * 'Quente' +
+        (temp > 35) * 'Muito quente')
     
+
+#7
